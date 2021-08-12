6c8a9ddbad8c793db7de7915d34db096d2df8b9a
CarlaCorreia93 <86625791+CarlaCorreia93@users.noreply.github.com>
2021-08-12 17:58:39 -0300

    Arquivo adicionado via upload

---
+++ Javacina/src/br/com/generation/javacina/Complicação.java
@@ -0,0 +1,17 @@
+package br.com.generation.javacina;
+
+public class Complicação extends Pessoa {
+
+public int laudo;
+	
+	public int getComplicavacia () {
+		return laudo;
+	}
+	
+	public void setComplicavacia (int laudo) {
+		this.laudo = laudo;
+	}
+	
+	
+}
+
